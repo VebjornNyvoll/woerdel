3df335683be06cb6c31d5e1f1d0bd36f666ee33f
Vebjørn Nyvoll <vebjorny@stud.ntnu.no>
2022-04-25 15:09:35 +0200

    first commit project

---
+++ src/main/java/Wordle/WordleController.java
@@ -0,0 +1,14 @@
+package Wordle;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class WordleController {
+    @FXML
+    private Button qkey, wkey, ekey, rkey, tkey, ykey, ukey, ikey, okey, pkey, akey, skey, dkey, fkey, gkey, hkey, jkey, kkey, lkey, zkey, xkey, ckey, vkey, bkey, nkey, mkey, enter, backspace;
+    
+    @FXML
+    private void handleQ(){
+        // Push letter Q to thing
+    }
+}
