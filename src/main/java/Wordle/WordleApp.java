684f61428eea709b3b494ed9fede40d71d6acae6
Vebjørn Nyvoll <vebjorn.nyvoll@gmail.com>
2023-09-30 09:31:25 +0200

    Readme changed slightly

--- src/main/java/Wordle/WordleApp.java
+++ src/main/java/Wordle/WordleApp.java
@@ -15,7 +15,7 @@ public class WordleApp extends Application{
     public static void main(String[] args) {
         Application.launch(args);
     }
-    
+
     @Override
     public void start(Stage primaryStage) throws IOException {
         primaryStage.setTitle("Woerdel");
